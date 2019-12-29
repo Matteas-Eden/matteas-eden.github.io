42d9354f158b0217cc374244110d70ac90e9f158
Matteas-Eden <mrmatteden@gmail.com>
2019-12-29 23:41:21 +1300

    Fix splash message

--- src/App.js
+++ src/App.js
@@ -7,9 +7,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <code>int main(int argc, char **argv) { puts("Hello World!"); }</code>
-        </p>
+        <pre>
+          <code>
+	  	puts("Hello World!");
+	  </code>
+        </pre>
         <a
           className="App-link"
           href="http://matteas-eden.github.io/jekyll-site"

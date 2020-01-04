a0f7545f39608b700056cc6f15cdea0b6aede8a4
Matteas-Eden <mrmatteden@gmail.com>
2020-01-04 16:15:53 +1300

    Remove example cards

--- src/Album.js
+++ src/Album.js
@@ -65,8 +65,6 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
-const cards = [2, 3, 4, 5, 6, 7, 8, 9];
-
 export default function Album() {
   const classes = useStyles();
   var keyIndex = 1;
@@ -274,34 +272,6 @@ export default function Album() {
                   </CardActions>
                 </Card>
             </Grid>
-            {/* Example */}
-            {cards.map(card => (
-              <Grid item key={card+10} xs={12} sm={6} md={4}>
-                <Card className={classes.card}>
-                  <CardMedia
-                    className={classes.cardMedia}
-                    image="https://source.unsplash.com/random"
-                    title="Image title"
-                  />
-                  <CardContent className={classes.cardContent}>
-                    <Typography gutterBottom variant="h5" component="h2">
-                      Heading
-                    </Typography>
-                    <Typography>
-                      This is a media card. You can use this section to describe the content.
-                    </Typography>
-                  </CardContent>
-                  <CardActions>
-                    <Button size="small" color="primary">
-                      View
-                    </Button>
-                    <Button size="small" color="primary">
-                      Edit
-                    </Button>
-                  </CardActions>
-                </Card>
-              </Grid>
-            ))}
           </Grid>
         </Container>
       </main>

0369e9f9402e6caefc03403580771ced30a9a9d3
Matteas-Eden <mrmatteden@gmail.com>
2020-02-26 16:28:19 +1300

    Revert from testing page to original draft

--- src/pages/Anzen.js
+++ src/pages/Anzen.js
@@ -4,19 +4,11 @@ import {Link as NavLink} from 'react-router-dom';
 class Anzen extends React.Component {
 	render() {
 		return(
-		<h1>
+		<div className="Anzen">
 			This is the Anzen page
-		</h1>
+		</div>
 		)
 	}
 }
 
-export default function Test() { 
-	return(
-		<React.Fragment>
-		<h1>This is a testing page</h1>
-		</React.Fragment>
-	)
-}
-
-// export default Anzen
+export default Anzen

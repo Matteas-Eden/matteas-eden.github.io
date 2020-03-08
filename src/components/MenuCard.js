95908e97215c926f06d7cf996ac48da441887099
Matt Eden <mrmatteden@gmail.com>
2020-03-09 08:29:30 +1300

    Re-order items; replace logo with text

--- src/components/MenuCard.js
+++ src/components/MenuCard.js
@@ -13,33 +13,39 @@ class MenuCard extends React.Component {
   render() {
     return (
       <div className="MenuCard">
-        <Grid container direction={'column'} spacing={3}>
-          <Grid item sm>
-          <img src={SignatureLogo} className="App-logo" alt="logo" />
-          </Grid>
-          <Divider />
-          <Grid item sm>
-            <Grid container direction={'column'} spacing={3}>
-              {/* <NavMenu items={["About", "Projects", "Resume", "Contact"]} /> */}
-              <Button component={Link} to={"/about"}>
-                About
-              </Button>
-              <Button component={Link} to={"/contact"}>
-                Contact
-              </Button>
-              <Button component={Link} to={"/cv"}>
-                CV
-              </Button>
-              <Button component={Link} to={"/projects"}>
-                Projects
-              </Button>
+        <div className="blurred-bg-container">
+          <Grid container direction={'column'} spacing={3}>
+            <Grid item sm>
+            <Button component={Link} to={"/"}>
+              {/* <img src={SignatureLogo} className="App-logo" alt="logo" /> */}
+              Home
+            </Button>
+            </Grid>
+            <Divider />
+            <Grid item sm>
+              <Grid container direction={'column'} spacing={3}>
+                {/* <NavMenu items={["About", "Projects", "Resume", "Contact"]} /> */}
+                <Button component={Link} to={"/about"}>
+                  About
+                </Button>
+                <Button component={Link} to={"/projects"}>
+                  Projects
+                </Button>
+                
+                <Button component={Link} to={"/cv"}>
+                  My CV
+                </Button>
+                <Button component={Link} to={"/contact"}>
+                  Contact
+                </Button>
+              </Grid>
+            </Grid>
+            <Divider />
+            <Grid item sm>
+              <LanguageSwitcher />
             </Grid>
           </Grid>
-          <Divider />
-          <Grid item sm>
-            <LanguageSwitcher />
-          </Grid>
-        </Grid>
+        </div>
       </div>
     );
   }

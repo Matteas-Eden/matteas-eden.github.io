aaa672e2ea0297a6dc0b29c1c280e2610790819f
Matt Eden <mrmatteden@gmail.com>
2020-03-08 23:37:14 +1300

    Temporarily replace NavMenu with hardcoded links

--- src/components/MenuCard.js
+++ src/components/MenuCard.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import NavMenu from './NavMenu';
 import LanguageSwitcher from './LanguageSwitcher';
-import { Grid, Box, Divider } from '@material-ui/core';
+import { Grid, Divider, Button } from '@material-ui/core';
 import SignatureLogo from '../logo.svg';
+import { Link } from 'react-router-dom';
 import '../menu.css';
 
 class MenuCard extends React.Component {
@@ -18,7 +19,21 @@ class MenuCard extends React.Component {
           </Grid>
           <Divider />
           <Grid item sm>
-            <NavMenu items={["About", "Projects", "Resume", "Contact"]} />
+            <Grid container direction={'column'} spacing={3}>
+              {/* <NavMenu items={["About", "Projects", "Resume", "Contact"]} /> */}
+              <Button component={Link} to={"/about"}>
+                About
+              </Button>
+              <Button component={Link} to={"/contact"}>
+                Contact
+              </Button>
+              <Button component={Link} to={"/cv"}>
+                CV
+              </Button>
+              <Button component={Link} to={"/projects"}>
+                Projects
+              </Button>
+            </Grid>
           </Grid>
           <Divider />
           <Grid item sm>

68938c7b6c389cb3f632b0758d8a2c55439f6266
Matt Eden <mrmatteden@gmail.com>
2020-03-10 07:21:14 +1300

    Adhere to jsx file convention

--- src/pages/Error404.jsx
+++ src/pages/Error404.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Typography from '@material-ui/core/Typography'
 
-export default function Error404(props) {
+export const Error404 = () => {
     return <Typography variant="h1">Error 404: Page Not Found</Typography>
 }

313d8baf227f4c1f3be6054357f6b60fee1cdb80
Matt Eden <mrmatteden@gmail.com>
2020-03-10 07:42:19 +1300

    Add typography

--- src/pages/Construction.jsx
+++ src/pages/Construction.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Typography } from '@material-ui/core';
 
 export const Construction = () => {
   return (
     <div className="construction">
-      <h1>This page is under construction. Please check back later.</h1>
+      <Typography variant ="h1">This page is under construction. Please check back later.</Typography>
     </div>
   );
 }

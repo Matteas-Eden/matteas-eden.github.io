2047a21277f87b93eb5f58df4317df2ecc512e5a
Matt Eden <mrmatteden@gmail.com>
2020-03-10 07:21:28 +1300

    Add 'under construction' page

---
+++ src/pages/Construction.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Construction = () => {
+  return (
+    <div className="construction">
+      This page is under construction. Please check back later.
+    </div>
+  );
+}

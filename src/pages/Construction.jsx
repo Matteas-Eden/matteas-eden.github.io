139d12dbb5b9b6537c7f16c1f3827eb06dcf3e9b
Matt Eden <mrmatteden@gmail.com>
2020-03-10 07:40:56 +1300

    Make message into header

--- src/pages/Construction.jsx
+++ src/pages/Construction.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const Construction = () => {
   return (
     <div className="construction">
-      This page is under construction. Please check back later.
+      <h1>This page is under construction. Please check back later.</h1>
     </div>
   );
 }

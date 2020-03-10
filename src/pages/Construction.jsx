fde5e6e54ca219176b014b98af14645bc86f9871
Matteas-Eden <mrmatteden@gmail.com>
2020-03-11 01:41:24 +1300

    Reduce size from h1 to h3

--- src/pages/Construction.jsx
+++ src/pages/Construction.jsx
@@ -4,7 +4,7 @@ import { Typography } from '@material-ui/core';
 export const Construction = () => {
   return (
     <div className="construction">
-      <Typography variant ="h1">This page is under construction. Please check back later.</Typography>
+      <Typography variant ="h3">This page is under construction. Please check back later.</Typography>
     </div>
   );
 }
--- src/pages/Error404.jsx
+++ src/pages/Error404.jsx
@@ -2,5 +2,5 @@ import React from 'react'
 import Typography from '@material-ui/core/Typography'
 
 export const Error404 = () => {
-    return <Typography variant="h1">Error 404: Page Not Found</Typography>
+    return <Typography variant="h3">Error 404: Page Not Found</Typography>
 }

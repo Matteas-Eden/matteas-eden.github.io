435a7ae12635ce3e713153e21f2743a8a0abf777
Matt Eden <mrmatteden@gmail.com>
2020-03-12 22:17:29 +1300

    Wrap message in Grid

--- src/pages/Construction.jsx
+++ src/pages/Construction.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import { Typography } from '@material-ui/core';
+import { Grid, Typography } from '@material-ui/core';
 
 export const Construction = () => {
   return (
     <div className="construction">
-      <Typography variant ="h3">This page is under construction. Please check back later.</Typography>
+      <Grid>
+        <Typography variant ="h3">This page is under construction. Please check back later.</Typography>
+      </Grid>
     </div>
   );
 }

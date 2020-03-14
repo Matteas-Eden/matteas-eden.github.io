e70824e3365db9c59e392f395339e7ac17189704
Matt Eden <mrmatteden@gmail.com>
2020-03-14 17:38:12 +1300

    Remove unnecessary files

--- src/pages/Error404.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import Typography from '@material-ui/core/Typography'
-
-export default function Error404(props) {
-    return <Typography variant="h1">Error 404: Page Not Found</Typography>
-}

326e2b4efabd35ab701f2d873917273b54415019
Matt Eden <mrmatteden@gmail.com>
2020-03-17 10:16:00 +1300

    Refactor out menu and content into components

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,26 +1,14 @@
 import React from 'react';
 
-import { Grid, Typography } from '@material-ui/core';
-import {
-    BrowserRouter as Router,
-    Route,
-    Switch,
-    Redirect,
-} from 'react-router-dom';
-
-// import { NavMenu } from '../components/NavMenu';
-import { Home } from './Home';
-import { About } from './About';
-import { Contact } from './Contact';
-import { CV } from './CV';
-import { Projects } from './Projects';
-
-// import { ReactComponent as Logo } from '../assets/images/SignatureBasic.svg';
-import { SignatureBasic } from '../components/SignatureBasic';
-import { SignatureCalligraphy } from '../components/SignatureCalligraphy';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Grid, useMediaQuery } from '@material-ui/core';
+import { Menu } from '../components/Menu';
+import { Content } from '../components/Content';
 import '../styles/main.css';
 
 export const Main = () => {
+    const matches = useMediaQuery('(min-width:600px) and (min-height:600px)');
+
     return (
         <Router>
             <div className="main">
@@ -31,35 +19,8 @@ export const Main = () => {
                     justify="center"
                     spacing={3}
                 >
-                    <div className="menu">
-                        <Typography variant="h3">Menu</Typography>
-                        <SignatureBasic size="10em" />
-                        <SignatureCalligraphy size="10em" />
-                        {/* <NavMenu /> */}
-                    </div>
-
-                    <div className="content">
-                        <div className="display-content">
-                            <Switch>
-                                <Route exact path="/">
-                                    <Home />
-                                </Route>
-                                <Route exact path="/about">
-                                    <About />
-                                </Route>
-                                <Route exact path="/projects">
-                                    <Projects />
-                                </Route>
-                                <Route exact path="/cv">
-                                    <CV />
-                                </Route>
-                                <Route exact path="/contact">
-                                    <Contact />
-                                </Route>
-                                <Redirect to="/" />
-                            </Switch>
-                        </div>
-                    </div>
+                    <Menu />
+                    {matches && <Content />}
                 </Grid>
             </div>
         </Router>

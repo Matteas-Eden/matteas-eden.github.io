2818fbf40be8b9c02279c5236c2f7688b1e802f0
Matt Eden <mrmatteden@gmail.com>
2020-03-19 23:41:02 +1300

    Add bg image; fix typo

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -4,23 +4,27 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { Grid, useMediaQuery } from '@material-ui/core';
 import { Menu } from '../components/Menu';
 import { Content } from '../components/Content';
-import '../styles/main.css';
+import '../styles/Main.scss';
 
 export const Main = () => {
     const matches = useMediaQuery('(min-width:600px) and (min-height:600px)');
+    const bgImage = `url(https://images4.alphacoders.com/746/thumb-1920-746386.png)`;
 
     return (
         <Router>
-            <div className="main">
+            <div
+                className="main has-frosted-glass"
+                style={{ backgroundImage: bgImage }}
+            >
                 <Grid
                     container
                     direction="row"
-                    align="center"
+                    alignItems="center"
                     justify="center"
                     spacing={3}
                 >
-                    <Menu />
-                    {matches && <Content />}
+                    <Menu bg={bgImage} />
+                    {matches && <Content bg={bgImage} />}
                 </Grid>
             </div>
         </Router>

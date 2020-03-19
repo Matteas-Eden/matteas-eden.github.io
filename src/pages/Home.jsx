1f31aea287171c2ba9ffaf4a2c282605005c5efa
Matt Eden <mrmatteden@gmail.com>
2020-03-20 01:12:41 +1300

    Add filler text to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,5 +2,14 @@ import React from 'react';
 import { Typography } from '@material-ui/core';
 
 export const Home = () => {
-    return <Typography variant="h3">Home</Typography>;
+    return (
+        <Typography variant="h3">
+            This website is under construction. Lorem ipsum dolor sit amet,
+            consectetur adipiscing elit. Nulla arcu massa, ornare vitae tempus
+            et, ultrices eget sapien. Ut leo nunc, iaculis ut congue id, congue
+            a justo. Nunc dui neque, porttitor ac elit nec, sodales tincidunt
+            urna. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Aliquam id auctor tortor, eget efficitur nunc.
+        </Typography>
+    );
 };

4143244f23540dc5301c1416d2a2635657821848
Matt Eden <mrmatteden@gmail.com>
2020-03-20 00:17:34 +1300

    Add frosted glass

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,35 +1,39 @@
 import React from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 
+import { FrostedGlassView } from '../components/FrostedGlassView';
+
 import { Home } from '../pages/Home';
 import { About } from '../pages/About';
 import { Contact } from '../pages/Contact';
 import { CV } from '../pages/CV';
 import { Projects } from '../pages/Projects';
 
-export const Content = () => {
+export const Content = props => {
     return (
-        <div className="content">
-            <div className="display-content">
-                <Switch>
-                    <Route exact path="/">
-                        <Home />
-                    </Route>
-                    <Route exact path="/about">
-                        <About />
-                    </Route>
-                    <Route exact path="/projects">
-                        <Projects />
-                    </Route>
-                    <Route exact path="/cv">
-                        <CV />
-                    </Route>
-                    <Route exact path="/contact">
-                        <Contact />
-                    </Route>
-                    <Redirect to="/" />
-                </Switch>
+        <FrostedGlassView bg={props.bg} className="my-glass">
+            <div className="content">
+                <div className="display-content">
+                    <Switch>
+                        <Route exact path="/">
+                            <Home />
+                        </Route>
+                        <Route exact path="/about">
+                            <About />
+                        </Route>
+                        <Route exact path="/projects">
+                            <Projects />
+                        </Route>
+                        <Route exact path="/cv">
+                            <CV />
+                        </Route>
+                        <Route exact path="/contact">
+                            <Contact />
+                        </Route>
+                        <Redirect to="/" />
+                    </Switch>
+                </div>
             </div>
-        </div>
+        </FrostedGlassView>
     );
 };

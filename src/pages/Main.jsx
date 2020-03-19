f80dcbd4a65af6095c27bc25fadab21b6a57e42d
Matt Eden <mrmatteden@gmail.com>
2020-03-20 00:16:23 +1300

    Use local image as background

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -5,26 +5,34 @@ import { Grid, useMediaQuery } from '@material-ui/core';
 import { Menu } from '../components/Menu';
 import { Content } from '../components/Content';
 import '../styles/Main.scss';
+import bgImage from '../assets/images/clay-banks-bg.jpg';
 
 export const Main = () => {
     const matches = useMediaQuery('(min-width:600px) and (min-height:600px)');
-    const bgImage = `url(https://images4.alphacoders.com/746/thumb-1920-746386.png)`;
+
+    const bgImageValue = `url(${bgImage})`;
 
     return (
         <Router>
             <div
                 className="main has-frosted-glass"
-                style={{ backgroundImage: bgImage }}
+                style={{ backgroundImage: bgImageValue }}
             >
                 <Grid
                     container
                     direction="row"
                     alignItems="center"
                     justify="center"
-                    spacing={3}
+                    spacing={10}
                 >
-                    <Menu bg={bgImage} />
-                    {matches && <Content bg={bgImage} />}
+                    <Grid item>
+                        <Menu bg={bgImageValue} />
+                    </Grid>
+                    {matches && (
+                        <Grid item>
+                            <Content bg={bgImageValue} />
+                        </Grid>
+                    )}
                 </Grid>
             </div>
         </Router>

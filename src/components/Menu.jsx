7ced82cffaf7d192d0e8e6f67142f356c0efcfea
Matt Eden <mrmatteden@gmail.com>
2020-03-20 00:18:28 +1300

    Move components; add frosted glass
    This commit encapsulates two changes:
    - Adding frosted glass to the menu
    - Moving components from NavMenu -> Menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
 
 import { NavMenu } from './NavMenu';
+import { FrostedGlassView } from './FrostedGlassView';
+import { LanguageSwitcher } from './LanguageSwitcher';
+import { SignatureCalligraphy } from './SignatureCalligraphy';
 
-export const Menu = () => {
+import { Grid } from '@material-ui/core';
+
+export const Menu = props => {
     return (
-        <div className="menu">
-            <NavMenu items={['About', 'Projects', 'My CV', 'Contact']} />
+        <div className="menu bg has-frosted-glass">
+            <FrostedGlassView bg={props.bg} className="my-glass">
+                <Grid
+                    container
+                    direction="column"
+                    alignItems="center"
+                    justify="center"
+                    spacing={3}
+                >
+                    <SignatureCalligraphy size="8em" />
+                    <NavMenu
+                        items={['About', 'Projects', 'My CV', 'Contact']}
+                    />
+                </Grid>
+            </FrostedGlassView>
         </div>
     );
 };
--- src/components/NavMenu.jsx
+++ src/components/NavMenu.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
-import MenuItem from './MenuItem';
+import { MenuItem } from './MenuItem';
 import { Grid } from '@material-ui/core';
-import { LanguageSwitcher } from './LanguageSwitcher';
-import { SignatureCalligraphy } from './SignatureCalligraphy';
 
 export const NavMenu = props => {
     const items = props.items;
     return (
         <div className="nav-menu">
             <Grid container direction="column" spacing={5}>
-                <SignatureCalligraphy size="100px" />
                 {items.map((item, index) => (
                     <MenuItem
                         key={index}
@@ -17,7 +14,6 @@ export const NavMenu = props => {
                         onClick={console.log('Clicked an item')}
                     />
                 ))}
-                <LanguageSwitcher />
             </Grid>
         </div>
     );

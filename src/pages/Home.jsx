38441b8e8b79c52108f894d54d4d1f2c8de181f9
Matt Eden <mrmatteden@gmail.com>
2020-03-25 01:18:02 +1300

    Add ENG/JAP description
    Adds a description of the site so that it can be deployed,
    but also so visitors understand it's incomplete and still being worked
    on.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,13 +3,30 @@ import { Typography } from '@material-ui/core';
 
 export const Home = () => {
     return (
-        <Typography style={{ fontFamily: 'high-tower' }} variant="h3">
-            This website is under construction. Lorem ipsum dolor sit amet,
-            consectetur adipiscing elit. Nulla arcu massa, ornare vitae tempus
-            et, ultrices eget sapien. Ut leo nunc, iaculis ut congue id, congue
-            a justo. Nunc dui neque, porttitor ac elit nec, sodales tincidunt
-            urna. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Aliquam id auctor tortor, eget efficitur nunc.
-        </Typography>
+        <div>
+            <Typography
+                style={{ fontFamily: 'high-tower', color: '#000' }}
+                variant="h3"
+            >
+                Hi, I'm Matthew Eden. This is my website.
+                <br />
+                It's still under construction at the moment, but make sure to
+                watch this space.
+                <br />
+            </Typography>
+            <Typography
+                style={{
+                    fontFamily: 'chogokubosogothic',
+                    fontWeight: 'bold',
+                    color: '#000',
+                }}
+                variant="h3"
+            >
+                <br />
+                こんにちは、イーデン・マシューです。私のサイトへようこそ。
+                <br />
+                現在まだ工事中だけど、きっとこのスペースを見続けて！
+            </Typography>
+        </div>
     );
 };

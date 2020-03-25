8591b6925a15dbe1d2550bd8c14bc612a0e78763
Matt Eden <mrmatteden@gmail.com>
2020-03-26 00:02:46 +1300

    Hardcode menu items and add location prop

--- src/components/NavMenu.jsx
+++ src/components/NavMenu.jsx
@@ -3,7 +3,7 @@ import { MenuItem } from './MenuItem';
 import { Grid } from '@material-ui/core';
 
 export const NavMenu = props => {
-    const items = props.items;
+    // const items = props.items;
     return (
         <div className="nav-menu">
             <Grid
@@ -12,13 +12,30 @@ export const NavMenu = props => {
                 alignItems="flex-start"
                 spacing={8}
             >
-                {items.map((item, index) => (
-                    <MenuItem
-                        key={index}
-                        item={item}
-                        onClick={console.log('Clicked an item')}
-                    />
-                ))}
+                <MenuItem
+                    key={0}
+                    item="About"
+                    onClick={console.log('Clicked an item')}
+                    location="/about"
+                />
+                <MenuItem
+                    key={1}
+                    item="Projects"
+                    onClick={console.log('Clicked an item')}
+                    location="/projects"
+                />
+                <MenuItem
+                    key={2}
+                    item="CV"
+                    onClick={console.log('Clicked an item')}
+                    location="/cv"
+                />
+                <MenuItem
+                    key={3}
+                    item="Contact"
+                    onClick={console.log('Clicked an item')}
+                    location="/contact"
+                />
             </Grid>
         </div>
     );

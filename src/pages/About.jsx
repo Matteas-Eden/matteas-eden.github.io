0a9f8105b3cf68ec9c01d3fd2e3deec25b4b5f3b
Matt Eden <mrmatteden@gmail.com>
2020-03-26 18:40:13 +1300

    Add some information about me for 'about' section

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,5 +2,25 @@ import React from 'react';
 import { Typography } from '@material-ui/core';
 
 export const About = () => {
-    return <Typography variant="h3">About me</Typography>;
+    return (
+        <div>
+            <Typography
+                style={{ fontFamily: 'high-tower', color: '#000' }}
+                variant="h4"
+            >
+                Hello, I'm Matthew Eden. Welcome to my website.
+                <br />
+                <br />
+                At the moment, I'm working through my fourth and final year of a
+                Computer Engineering (Honours) degree at the University of
+                Auckland in New Zealand.
+                <br />
+                But of course, I'm also pursuing graduate work opportunities as
+                I look to begin my career in Software Development.
+                <br />I have always been passionate in all that I do, whether
+                it's my accademic studies, my extracurricular commitments or my
+                work in industry - I always strive to give 110%.
+            </Typography>
+        </div>
+    );
 };

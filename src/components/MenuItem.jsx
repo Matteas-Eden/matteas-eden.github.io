db409cb7ffe126b118cfae84d29df59ef913641a
Matt Eden <mrmatteden@gmail.com>
2020-03-26 16:38:59 +1300

    Combine Link and Button components

--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -13,17 +13,12 @@ export const MenuItem = props => {
                 height: '4rem',
                 marginRight: '3em',
             }}
+            component={Link}
+            to={props.location}
         >
-            <Link
-                to={props.location}
-                style={{ textDecoration: 'none', color: 'inherit' }}
-            >
-                <Typography
-                    style={{ fontFamily: 'high-tower', fontSize: '4rem' }}
-                >
-                    {props.item}
-                </Typography>
-            </Link>
+            <Typography style={{ fontFamily: 'high-tower', fontSize: '4rem' }}>
+                {props.item}
+            </Typography>
         </Button>
     );
 };

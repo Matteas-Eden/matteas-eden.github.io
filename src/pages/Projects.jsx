bf6e5504ba1d878bb475f8461132b9ddd5a8fb84
Matt Eden <mrmatteden@gmail.com>
2020-03-27 22:40:12 +1300

    Add basic layout for project cards

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,6 +1,47 @@
 import React from 'react';
-import { Typography } from '@material-ui/core';
+import { Grid, Typography } from '@material-ui/core';
+import { ProjectCard } from '../components/ProjectCard';
+import pic from '../assets/images/TokyoStreet.png';
 
 export const Projects = () => {
-    return <Typography variant="h3">Projects</Typography>;
+    return (
+        <div className="project-page">
+            <Typography
+                style={{ fontFamily: 'high-tower', color: '#000' }}
+                variant="h3"
+            >
+                Never Stop Creating
+            </Typography>
+            <Grid container spacing={6} alignItems="center">
+                <Grid item xs={6}>
+                    <ProjectCard
+                        image={pic}
+                        title="Title 1"
+                        description="Example description"
+                    />
+                </Grid>
+                <Grid item xs={6}>
+                    <ProjectCard
+                        image={pic}
+                        title="Title 2"
+                        description="Example description"
+                    />
+                </Grid>
+                <Grid item xs={6}>
+                    <ProjectCard
+                        image={pic}
+                        title="Title 3"
+                        description="Example description"
+                    />
+                </Grid>
+                <Grid item xs={6}>
+                    <ProjectCard
+                        image={pic}
+                        title="Title 4"
+                        description="Example description"
+                    />
+                </Grid>
+            </Grid>
+        </div>
+    );
 };

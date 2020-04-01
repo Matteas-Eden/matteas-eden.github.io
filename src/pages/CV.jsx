289fd5a494b0de25a2df9dd7a527f856a4ce3f5c
Matt Eden <mrmatteden@gmail.com>
2020-04-01 13:58:42 +1300

    Rename CV to Resume

--- src/pages/CV.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { Typography } from '@material-ui/core';
-
-export const CV = () => {
-    return <Typography variant="h3">My CV</Typography>;
-};
---
+++ src/pages/Resume.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Typography } from '@material-ui/core';
+
+export const Resume = () => {
+    return <Typography variant="h3">Resumé</Typography>;
+};

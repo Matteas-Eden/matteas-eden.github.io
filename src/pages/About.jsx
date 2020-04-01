b3d8877bd079300e42f253ca9f6a371ce014e9e5
Matt Eden <mrmatteden@gmail.com>
2020-04-01 14:01:01 +1300

    Change 'about' section; remove black line

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -9,7 +9,7 @@ export const About = () => {
                 style={{ fontFamily: 'high-tower', color: '#000' }}
                 variant="h4"
             >
-                Hello, I'm Matthew 'Matteas' Eden. Welcome to my website.
+                Hi, I'm Matteas Eden. Welcome to my website.
                 <br />
                 <br />
                 At the moment, I'm working through my fourth and final year of a
@@ -25,7 +25,7 @@ export const About = () => {
                 If you'd like to learn more about me, take a look around the
                 website and at the links below.
             </Typography>
-            <ColouredLine color="black" />
+            {/* <ColouredLine color="black" /> */}
         </div>
     );
 };

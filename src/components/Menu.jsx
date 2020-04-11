34ce462142366a2cedd741463e38d7cc2ea6ed40
Matt Eden <mrmatteden@gmail.com>
2020-04-12 01:56:53 +1200

    Deprecate ColouredLine component

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -2,12 +2,10 @@ import React from 'react';
 
 import { NavMenu } from './NavMenu';
 import { FrostedGlassView } from './FrostedGlassView';
-import { ColouredLine } from './ColouredLine';
-// import { LanguageSwitcher } from './LanguageSwitcher';
 import { SignatureCalligraphy } from './SignatureCalligraphy';
 import { Construction } from '../pages/Construction';
 
-import { Grid, Button } from '@material-ui/core';
+import { Button } from '@material-ui/core';
 import { Link } from 'react-router-dom';
 
 export const Menu = props => {

471d58c969986048258a8b6dfa326be6fa08f291
Matt Eden <mrmatteden@gmail.com>
2020-04-12 01:49:23 +1200

    Refactor implementation to use map
    Also cleans up styling

--- src/components/NavMenu.jsx
+++ src/components/NavMenu.jsx
@@ -3,39 +3,21 @@ import { MenuItem } from './MenuItem';
 import { Grid } from '@material-ui/core';
 
 export const NavMenu = props => {
-    // const items = props.items;
     return (
         <div className="nav-menu">
             <Grid
                 container
                 direction="column"
                 alignItems="flex-start"
-                spacing={8}
+                justify="flex-start"
             >
-                <MenuItem
-                    key={0}
-                    item="About"
-                    onClick={console.log('Clicked an item')}
-                    location="/about"
-                />
-                <MenuItem
-                    key={1}
-                    item="Projects"
-                    onClick={console.log('Clicked an item')}
-                    location="/projects"
-                />
-                <MenuItem
-                    key={2}
-                    item="Resumé"
-                    onClick={console.log('Clicked an item')}
-                    location="/resume"
-                />
-                <MenuItem
-                    key={3}
-                    item="Contact"
-                    onClick={console.log('Clicked an item')}
-                    location="/contact"
-                />
+                {props.items.map((item, key) => (
+                    <MenuItem
+                        key={key}
+                        item={item.text}
+                        location={item.location}
+                    />
+                ))}
             </Grid>
         </div>
     );

97ebff807432603a6118f0e3eae0e1f5720af7ec
Matt Eden <mrmatteden@gmail.com>
2020-04-11 20:47:12 +1200

    Create simple resume page

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
-import { Typography, makeStyles } from '@material-ui/core';
+import { Grid, Typography, makeStyles } from '@material-ui/core';
+import ResumeImage from '../assets/images/resume.png';
+import ResumeFile from '../assets/resume.pdf';
+import { Label } from '../components/Label';
+import { mdiDownload } from '@mdi/js';
 
 const useStyles = makeStyles({
     root: {
+        position: 'relative',
+    },
+    text: {
         fontFamily: 'high-tower',
         color: '#000',
         fontSize: '10vmin',
+    },
+    image: {
+        position: 'absolute',
+        right: '1vh',
+        border: '1px solid black',
+        height: '75vh',
     },
 });
 
@@ -13,17 +26,38 @@ export const Resume = () => {
     const classes = useStyles();
 
     return (
-        <div>
-            <Typography className={classes.root} variant="h3">
-                Resumé
-            </Typography>
-            <Typography
-                className={classes.root}
-                style={{ fontSize: '5vmin' }}
-                variant="h4"
+        <div className={classes.root}>
+            <Grid
+                container
+                direction="row"
+                alignItems="stretch"
+                justify="flex-start"
             >
-                This page is still under construction.
-            </Typography>
+                <Grid item>
+                    <Typography className={classes.text} variant="h3">
+                        Resumé
+                    </Typography>
+                    {/* <Typography
+                        className={classes.text}
+                        style={{ fontSize: '5vmin' }}
+                        variant="h4"
+                    >
+                        This page is still under construction.
+                    </Typography> */}
+                    <Label
+                        text="Download"
+                        link={ResumeFile}
+                        img={mdiDownload}
+                    />
+                </Grid>
+                <Grid item>
+                    <img
+                        className={classes.image}
+                        src={ResumeImage}
+                        alt="resumé"
+                    />
+                </Grid>
+            </Grid>
         </div>
     );
 };

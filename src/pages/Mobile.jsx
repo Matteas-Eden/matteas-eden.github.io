707d1da39daef840b76a6819c92e5c8466330fe7
Matt Eden <mrmatteden@gmail.com>
2020-04-13 22:34:14 +1200

    Add working navigation header

--- src/pages/Mobile.jsx
+++ src/pages/Mobile.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
-import { Grid, Typography, makeStyles } from '@material-ui/core';
-import { MobileHeader } from '../components/MobileHeader';
+import {
+    Grid,
+    Typography,
+    makeStyles,
+    AppBar,
+    Select,
+    MenuItem,
+} from '@material-ui/core';
 
 import { Route, Switch, Redirect } from 'react-router-dom';
 
@@ -14,12 +20,54 @@ import { Projects } from '../pages/Projects';
 
 import { Footer } from '../components/Footer';
 
+import { useHistory } from 'react-router-dom';
+
+const MobileHeader = props => {
+    let history = useHistory();
+    return (
+        <AppBar
+            position="static"
+            color="transparent"
+            style={{
+                // position: 'absolute',
+                // top: '0',
+                border: '1px solid black',
+                shadows: 'none',
+                height: '4vh',
+            }}
+        >
+            <Select
+                native
+                autoWidth
+                defaultValue="Home"
+                style={{ height: '4vh' }}
+                onChange={event => {
+                    let item = event.target.value;
+                    console.log(item);
+                    if (item === 'Home') history.push('/');
+                    else if (item === 'Resumé') history.push('/resume');
+                    else history.push('/' + item.toLowerCase());
+                }}
+            >
+                {['Home', 'About', 'Projects', 'Resumé', 'Contact'].map(
+                    page => (
+                        <option>{page}</option>
+                    )
+                )}
+            </Select>
+        </AppBar>
+    );
+};
+
 export const Mobile = props => {
     return (
         <div className="mobile-content card">
             <FrostedGlassView bg={props.bg} className="my-glass">
                 <MobileHeader />
-                <div className="display-content">
+                <div
+                    className="display-content"
+                    style={{ position: 'absolute', top: '4vh' }}
+                >
                     <Switch>
                         <Route exact path="/">
                             <Home />

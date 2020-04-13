684e657fbfd23838e13dbfa7dd17d8585a0ca323
Matt Eden <mrmatteden@gmail.com>
2020-04-13 20:22:14 +1200

    Update about me blurb

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,7 +5,7 @@ const useStyles = makeStyles({
     root: {
         fontFamily: 'high-tower',
         color: '#000',
-        fontSize: '5vmin',
+        fontSize: '2.5vw',
     },
 });
 
@@ -15,7 +15,8 @@ export const About = () => {
     return (
         <div>
             <Typography className={classes.root} variant="h4">
-                Hi, I'm Matteas Eden. Welcome to my website.
+                My name is Matteas Eden and I'm a University student from New
+                Zealand. Welcome to my website.
                 <br />
                 <br />
                 At the moment, I'm working through my fourth and final year of a
@@ -27,9 +28,6 @@ export const About = () => {
                 <br />I have always been passionate in all that I do, whether
                 it's my accademic studies, my extracurricular commitments or my
                 work in industry - I always strive to give 110%.
-                <br />
-                If you'd like to learn more about me, take a look around the
-                website and at the links below.
             </Typography>
         </div>
     );

bbb88048224f8a0c407442330f837546918a88a5
Matt Eden <mrmatteden@gmail.com>
2020-04-14 00:26:25 +1200

    Trim grid props

--- src/components/NavMenu.jsx
+++ src/components/NavMenu.jsx
@@ -5,12 +5,7 @@ import { Grid } from '@material-ui/core';
 export const NavMenu = props => {
     return (
         <div className="nav-menu">
-            <Grid
-                container
-                direction="column"
-                alignItems="flex-start"
-                justify="flex-start"
-            >
+            <Grid container direction="column" alignItems="flex-start">
                 {props.items.map((item, key) => (
                     <MenuItem
                         key={key}

2f3f31237852bd5f99be7102783b72541e92b7b0
Matteas-Eden <45587386+Matteas-Eden@users.noreply.github.com>
2020-04-15 06:26:31 +0000

    Deploying to master from  - 0fdfc467b0f8da83c0269c6eeac7bfbe69aa9a41 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b7583c7c3c95fae756ab328d81873037.js"
+  "/precache-manifest.072f48bc63eaa842e4ddca8d72b41bf8.js"
 );
 
 self.addEventListener('message', (event) => {

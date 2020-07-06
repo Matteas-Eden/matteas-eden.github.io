Matteas-Eden/matteas-eden.github.io

commit 8010c7933a56a3600582288670edd1d96418fe21

Refactor routing in Content component

src/components/Content.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
-import { Route, Switch, Redirect } from 'react-router-dom';
+import {
+    BrowserRouter as Router,
+    Route,
+    Switch,
+    Redirect,
+} from 'react-router-dom';
 
 import { FrostedGlassView } from '../components/FrostedGlassView';
 
@@ -13,30 +18,26 @@ import { Footer } from '../components/Footer';
 
 export const Content = props => {
     return (
-        <div className="content card">
-            <FrostedGlassView bg={props.bg} className="my-glass">
-                <div className="display-content">
-                    <Switch>
-                        <Route exact path="/">
-                            <Home />
-                        </Route>
-                        <Route exact path="/about">
-                            <About />
-                        </Route>
-                        <Route exact path="/projects">
-                            <Projects />
-                        </Route>
-                        <Route exact path="/resume">
-                            <Resume />
-                        </Route>
-                        <Route exact path="/contact">
-                            <Contact />
-                        </Route>
-                        <Redirect to="/" />
-                    </Switch>
-                    <Footer />
-                </div>
-            </FrostedGlassView>
-        </div>
+        <Router>
+            <div className="content card">
+                <FrostedGlassView bg={props.bg} className="my-glass">
+                    <div className="display-content">
+                        <Switch>
+                            <Route exact path="/" component={Home} />
+                            <Route exact path="/about" component={About} />
+                            <Route
+                                exact
+                                path="/projects"
+                                component={Projects}
+                            />
+                            <Route exact path="/resume" component={Resume} />
+                            <Route exact path="/contact" component={Contact} />
+                            <Redirect to="/" />
+                        </Switch>
+                        <Footer />
+                    </div>
+                </FrostedGlassView>
+            </div>
+        </Router>
     );
 };

commit 421304e04b14a98c1fde8b85b9fbd2ade6d4ecad

Fix routing issue caused by 8010c79

src/components/Content.jsx
@@ -1,10 +1,5 @@
 import React from 'react';
-import {
-    BrowserRouter as Router,
-    Route,
-    Switch,
-    Redirect,
-} from 'react-router-dom';
+import { Route, Switch, Redirect, withRouter } from 'react-router-dom';
 
 import { FrostedGlassView } from '../components/FrostedGlassView';
 
@@ -16,28 +11,24 @@ import { Projects } from '../pages/Projects';
 
 import { Footer } from '../components/Footer';
 
-export const Content = props => {
+export const ContentComponent = props => {
     return (
-        <Router>
-            <div className="content card">
-                <FrostedGlassView bg={props.bg} className="my-glass">
-                    <div className="display-content">
-                        <Switch>
-                            <Route exact path="/" component={Home} />
-                            <Route exact path="/about" component={About} />
-                            <Route
-                                exact
-                                path="/projects"
-                                component={Projects}
-                            />
-                            <Route exact path="/resume" component={Resume} />
-                            <Route exact path="/contact" component={Contact} />
-                            <Redirect to="/" />
-                        </Switch>
-                        <Footer />
-                    </div>
-                </FrostedGlassView>
-            </div>
-        </Router>
+        <div className="content card">
+            <FrostedGlassView bg={props.bg} className="my-glass">
+                <div className="display-content">
+                    <Switch>
+                        <Route exact path="/" component={Home} />
+                        <Route exact path="/about" component={About} />
+                        <Route exact path="/projects" component={Projects} />
+                        <Route exact path="/resume" component={Resume} />
+                        <Route exact path="/contact" component={Contact} />
+                        <Redirect to="/" />
+                    </Switch>
+                    <Footer />
+                </div>
+            </FrostedGlassView>
+        </div>
     );
 };
+
+export const Content = withRouter(ContentComponent);
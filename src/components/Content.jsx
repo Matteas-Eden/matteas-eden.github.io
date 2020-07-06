421304e04b14a98c1fde8b85b9fbd2ade6d4ecad
Matt Eden <mrmatteden@gmail.com>
2020-07-07 11:00:37 +1200

    Fix routing issue caused by 8010c79

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,10 +1,5 @@
 import React from 'react';
-import {
-    BrowserRouter as Router,
-    Route,
-    Switch,
-    Redirect,
-} from 'react-router-dom';
+import { Route, Switch, Redirect, withRouter } from 'react-router-dom';
 
 import { FrostedGlassView } from '../components/FrostedGlassView';
 
@@ -16,28 +11,24 @@ import { Projects } from '../pages/Projects';
 
 import { Footer } from '../components/Footer';
 
-export const Content = props => {
+export const ContentComponent = props => {
     return (
-        <Router>
-            <div className="content card">
-                <FrostedGlassView bg={props.bg} className="my-glass">
-                    <div className="display-content">
-                        <Switch>
-                            <Route exact path="/" component={Home} />
-                            <Route exact path="/about" component={About} />
-                            <Route
-                                exact
-                                path="/projects"
-                                component={Projects}
-                            />
-                            <Route exact path="/resume" component={Resume} />
-                            <Route exact path="/contact" component={Contact} />
-                            <Redirect to="/" />
-                        </Switch>
-                        <Footer />
-                    </div>
-                </FrostedGlassView>
-            </div>
-        </Router>
+        <div className="content card">
+            <FrostedGlassView bg={props.bg} className="my-glass">
+                <div className="display-content">
+                    <Switch>
+                        <Route exact path="/" component={Home} />
+                        <Route exact path="/about" component={About} />
+                        <Route exact path="/projects" component={Projects} />
+                        <Route exact path="/resume" component={Resume} />
+                        <Route exact path="/contact" component={Contact} />
+                        <Redirect to="/" />
+                    </Switch>
+                    <Footer />
+                </div>
+            </FrostedGlassView>
+        </div>
     );
 };
+
+export const Content = withRouter(ContentComponent);
